4587bf1abe5ad9bc5636e3703ebc734ea849a6bd
venkatbalaji <venkatbalaji9698@gmail.com>
2024-02-25 18:25:50 +0530

    search input integration

--- src/constants/url-constants.js
+++ src/constants/url-constants.js
@@ -1,7 +1,8 @@
 const BASE_URL = 'https://content.guardianapis.com/';
 
 export const UrlConstants = {
-    SEARCH: `${BASE_URL}search`
+    SEARCH: `${BASE_URL}search`,
+    TAGS: `${BASE_URL}tags`,
 }
 
 export default UrlConstants;
--- src/utils/api.js
+++ src/utils/api.js
@@ -3,13 +3,29 @@ import UrlConstants from "../constants/url-constants";
 
 export const getStoriesList = async (searchTerm, page, pageSize, showtags) => {
   try {
-    let url = `${UrlConstants.SEARCH}?api-key=test&show-fields=thumbnail,headline&q=${searchTerm}&show-tags=${showtags}&page=${page}&page-size=${pageSize}`;
-    const response = await axios.get(url);
-    return response.data.response;
+    const url = `${UrlConstants.SEARCH}?api-key=test&show-fields=thumbnail,headline&q=${searchTerm}&show-tags=${showtags}&page=${page}&page-size=${pageSize}`;
+    const res = await axios.get(url);
+    return res.data.response;
   } catch (er) {
     console.log(er);
     return {
       status: "error",
     };
+  }
+};
+
+export const getSuggestionList = async (searchTerm) => {
+  try {
+    const url = `${UrlConstants.TAGS}?show-references=all&api-key=test&q=${searchTerm}`;
+    const res = await axios.get(url);
+
+    if (res?.data?.response?.results) {
+      return res.data.response.results.filter((a) => a.type === "keyword");
+    }
+
+    return [];
+  } catch (er) {
+    console.log(er);
+    return [];
   }
 };
